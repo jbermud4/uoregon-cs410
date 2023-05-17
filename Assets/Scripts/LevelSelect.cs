7a97e1c1c88c63bf526d13624eb818a0abe64c80
Uristthedorf <40842973+Uristthedorf@users.noreply.github.com>
2023-05-16 22:40:31 -0700

    Credit

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//https://www.youtube.com/watch?v=-GWjA6dixV4&ab_channel=BMo as credit for main menu stuff.
+
 public class LevelSelect : MonoBehaviour
 {
     public void loadLevelOne()
