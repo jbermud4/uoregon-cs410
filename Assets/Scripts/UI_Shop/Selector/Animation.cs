9b5e86642ce93d732d925962df87c8d7d970e4df
ethanhyde <ehyde9@gmail.com>
2023-06-07 19:14:56 -0700

    Selector progress

---
+++ Assets/Scripts/UI_Shop/Selector/Animation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Vector3 initPosition;
+    [SerializeField] private Vector3 finalPosition;
+
+    private void Awake()
+    {
+        initPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, finalPosition, 0.1f);
+    }
+
+    private void OnDisable()
+    {
+        transform.position = initPosition;
+    }
+}
+
+
